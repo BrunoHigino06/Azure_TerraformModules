8d95fc22a0ca454e02a7893f47e25ffa16c91d56
bruno <brunohigino06@gmail.com>
2024-05-08 16:39:08 -0300

    fix the resource group module

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "resource_group" {
   type          = list(object({
     name        = string
     location    = string
-    managed_by  = optiona(string)
+    managed_by  = optional(string)
     tags        = map(string)
   }))
 }
