1e30909b79c24f023bbf638572350f3c681ecd6a
bruno <brunohigino06@gmail.com>
2024-05-16 10:51:20 -0300

    change the address space and dns server to allow a list of ip's

--- variable.tf
+++ variable.tf
@@ -2,14 +2,14 @@ variable "vnet" {
   type                          = list(object({
     name                        = string 
     resource_group_name         = string
-    address_space               = string
+    address_space               = list(string)
     location                    = string
     tags                        = map(string)
     bgp_community               = optional(string)
     ddos_protection_plan_id     = optional(string)
     ddos_protection_plan_enable = optional(string)
     encryption_enforcement      = optional(string)
-    dns_servers                 = optional(string)
+    dns_servers                 = optional(list(string))
     edge_zone                   = optional(string)
     flow_timeout_in_minutes     = optional(string)
     subnet                      = optional(list(object({
