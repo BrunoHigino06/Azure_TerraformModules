f3f5aababd4fd21c0fc7542935fdae2806c45366
bruno <brunohigino06@gmail.com>
2024-05-16 11:27:17 -0300

    organize the subnet module

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "azurerm_subnet" "subnet" {
     count = length(var.subnet)      
-    name                 = var.subnet[count.index].name
-    resource_group_name  = var.subnet[count.index].resource_group_name
-    virtual_network_name = var.subnet[count.index].virtual_network_name
-    address_prefixes     = var.subnet[count.index].address_prefixes
-    private_link_service_network_policies_enabled = var.subnet[count.index].private_link_service_network_policies_enabled
-    service_endpoints = var.subnet[count.index].service_endpoints
-    service_endpoint_policy_ids= var.subnet[count.index].service_endpoint_policy_ids
+    name                                            = var.subnet[count.index].name
+    resource_group_name                             = var.subnet[count.index].resource_group_name
+    virtual_network_name                            = var.subnet[count.index].virtual_network_name
+    address_prefixes                                = var.subnet[count.index].address_prefixes
+    private_link_service_network_policies_enabled   = var.subnet[count.index].private_link_service_network_policies_enabled
+    service_endpoints                               = var.subnet[count.index].service_endpoints
+    service_endpoint_policy_ids                     = var.subnet[count.index].service_endpoint_policy_ids
     delegation {
-        name = var.subnet[count.index].delegation_name
+        name                                        = var.subnet[count.index].delegation_name
         service_delegation {
-        name    = var.subnet[count.index].service_delegation_name
-        actions = var.subnet[count.index].service_delegation_actions
+        name                                        = var.subnet[count.index].service_delegation_name
+        actions                                     = var.subnet[count.index].service_delegation_actions
         }
     }
 }
--- variable.tf
+++ variable.tf
@@ -1,15 +1,14 @@
 variable "subnet" {
-  type = list(object({
-    name = string
-    resource_group_name = string
-    virtual_network_name = string
-    address_prefixes = list(string)
-    delegation_name = optional(string)
-    service_delegation_name = optional(string)
-    service_delegation_actions = optional(list(string))
-    private_endpoint_network_policies = optional(string)
+  type                                            = list(object({
+    name                                          = string
+    resource_group_name                           = string
+    virtual_network_name                          = string
+    address_prefixes                              = list(string)
+    delegation_name                               = optional(string)
+    service_delegation_name                       = optional(string)
+    service_delegation_actions                    = optional(list(string))
     private_link_service_network_policies_enabled = optional(string)
-    service_endpoints = optional(list(string))
-    service_endpoint_policy_ids = optional(list(string))
+    service_endpoints                             = optional(list(string))
+    service_endpoint_policy_ids                   = optional(list(string))
   }))
 }
